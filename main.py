5d90927f57c8b50dc689749a1055c4fbb5df4fd4
theshadycupcake <vil.salmensaari@gmail.com>
2022-01-24 10:53:46 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import sys, time
+#is just useful stuff to have
+addition = "addition"
+#variable joka tekee ohjelmasta enemmän user friendly
+factoriall = "factorial"
+#sama kun yläpuolella
+yes = ["Yes", "1. Yes"]
+list = []
+#lista vastausten tallentamissen ja .txt documenttiin printtaamiseen jos käyttäjä haluaa
+def f_print(srt):
+    for let in srt:
+        sys.stdout.write(let)
+        sys.stdout.flush()
+        time.sleep(0.05)
+# mukavamman näköistä teksti printtausta
+
+def factorial(x):
+    fact = 1
+    for num in range(2, x +1):
+        fact *= num
+    return fact
+#kertoma funtio koska miksi ei0
+#joka ei nököjään halua toimia
+#wip
+f_print("choose the operation")
+print("")
+f_print("1. addition")
+print("")
+f_print("2. substraction")
+print("")
+f_print("3. factorial")
+print("")
+f_print("0 to exit")
+while True:
+    print("")
+    choice = input("operation: ")
+    if choice == 1 or choice.lower() == addition:
+        f_print("jaa")
+    if choice == "3" or choice.lower() == factoriall:
+        while True:
+            try:
+                number = int(input(f_print("Num: ")))
+                #note to self, try to make f_print work with input without writing None
+            except ValueError:
+                print("please give a number")
+                continue
+            else:
+                break
+        factnum = factorial(number)
+        f_print(f"Factorial of {number} is {factnum}")
+        var1 = "factorial of " + str(number) + " is " + str(factnum)
+        list.append(var1)
+        #stuff happens here, dont ask me wat
+    if choice == "0":
+        break
+print(list)
+f_print("Would you like to save your answers to a txxt document")
+print("")
+f_print("1. Yes")
+print("")
+f_print("2. No")
+print("")
+while True:
+    #tektsi tiedostoon tallentaminen
+    ans = input(("Answer: "))
+    if ans == 1 or ans in yes:
+        print("ok")
+        print("")
+        f = open("ans.txt", "w+")
+        #magic
+        for item in list:
+            f.write(item+ "\n")
+        f.close
+        break
+    else: 
+        f_print("please answer correctly")
+        print("")
+        continue
+print("Thank you for using this programm")
